f190751668861a672bafa25faaa3e59fb0e714f5
Swapnil Rao <swapnil.rao1@gmail.com>
2023-11-22 01:03:25 +0530

    first mole/message added

--- AT.js
+++ AT.js
@@ -2,7 +2,7 @@ let moletile
 
 
 window.onload=function(){
-    setGame();
+    game();
 }
 
 function game(){
@@ -10,6 +10,24 @@ function game(){
         let tile=document.createElement("div")
         tile.id=i.toString()
         document.getElementById("surface").appendChild(tile)
-    }     
+    }  
+    
+    setInterval(setmole1,1000)
 }
 
+function randomint(){
+    let num=Math.floor(Math.random()*9);
+    return num.toString()
+}
+
+function setmole1(){
+    if(moletile){
+        moletile.innerHTML=""
+    } 
+    let mole=document.createElement('img')
+    mole.src='bg3b.png'
+
+    let num=randomint();
+    moletile=document.getElementById(num)
+    moletile.appendChild(mole)
+}
